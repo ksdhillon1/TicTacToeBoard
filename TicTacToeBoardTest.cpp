24beeb482bdb92ad9d4346ead9f3a5ccadb69523
Karandeep <ksdhillon1@mail.csuchico.edu>
2022-11-16 11:46:08 -0800

    added toggleturn testing

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -4,15 +4,44 @@
 
 #include <gtest/gtest.h>
 #include "TicTacToeBoard.h"
- 
+
 class TicTacToeBoardTest : public ::testing::Test
 {
 	protected:
-		TicTacToeBoardTest(){} 
-		virtual ~TicTacToeBoardTest(){} 
+		TicTacToeBoardTest(){}
+		virtual ~TicTacToeBoardTest(){}
 		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
+TEST(TicTacToeBoardTest, smoke_test)
+{
+    ASSERT_TRUE(true);
+}
 
+TEST(TicTacToeBoardTest, invalid_test)
+{
+   TicTacToeBoard test;
+   Piece test1;
 
+   test1 = test.toggleTurn();
+
+    ASSERT_EQ(test1, O);
+}
+
+TEST(TicTacToeBoardTest, test2)
+{
+   TicTacToeBoard test;
+   Piece test1;
+
+   test1 = test.toggleTurn();
+   test1 = test.toggleTurn();
+    ASSERT_EQ(test1, X);
+}
+
+TEST(TicTacToeBoardTest, init_test)
+{
+   TicTacToeBoard test;
+   Piece test1;
+    ASSERT_EQ(test1, Blank);
+}
